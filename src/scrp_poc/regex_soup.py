b609455d2f090340e95539a012387ab7fee31ff9
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-08-02 11:24:44 +0200

    feat: scrape name and favorite color

--- src/scrp_poc/regex_soup.py
+++ src/scrp_poc/regex_soup.py
@@ -24,3 +24,25 @@ def print_results():
 
     click.echo(f"***Regex results from {URL}***")
     click.echo(f"title: {title}")
+    click.echo(f"Found name: {find_text(html, 'Name: ')}")
+    click.echo(f"Found favourite coulour: {find_text(html, 'Favorite Color:')}")
+
+
+def find_text(raw_html, text_to_find):
+    """
+    Finds and extracts text content from HTML.
+
+    Args:
+        html (str): The HTML content to search within.
+        text_to_find (str): The text to locate within the HTML.
+
+    Returns:
+        str: The extracted text content stripped of leading and trailing whitespace characters.
+    """
+
+    text_index = raw_html.find(text_to_find)
+    start_index = text_index + len(text_to_find)
+    next_html_tag_offset = raw_html[start_index:].find("</")
+    end_index = start_index + next_html_tag_offset
+    raw_text = raw_html[start_index:end_index]
+    return raw_text.strip(" \r\n\t")
