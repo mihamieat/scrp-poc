9497a7acd9fae2a1ca3eed6aafa163f4dc47ced5
Mihamina Rakotovazaha <r.miham@yahoo.com>
2024-08-05 16:34:19 +0200

    feat: grab full html from a page

--- src/scrp_poc/beauty_soup.py
+++ src/scrp_poc/beauty_soup.py
@@ -11,6 +11,15 @@ with urlopen(URL) as page:
 soup = BeautifulSoup(html, "html.parser")
 image1, image2 = soup.find_all("img")
 
+# Grab the full HTML from the page at the URL
+URL2 = "http://olympus.realpython.org/profiles"
+with urlopen(URL2) as page2:
+    html2 = page2.read().decode("utf-8")
+soup2 = BeautifulSoup(html2, "html.parser")
+hyperlinks = soup2.find_all("a")
+hrefs = [a["href"] for a in hyperlinks]
+links = [URL2 + href for href in hrefs]
+
 
 def print_parser():
     """
@@ -29,3 +38,4 @@ def print_parser():
     click.echo(f"{image1.name} {image2.name}")
     click.echo(f"{image1['src']} {image2['src']}")
     click.echo(f"Title: {soup.title.string}")
+    click.echo("\n".join(links))
